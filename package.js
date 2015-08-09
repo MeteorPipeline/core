415ca391f0a2b315ff4e890b1f4eef72eef27b6c
Tim Brandin <tim.brandin@gmail.com>
2015-08-10 01:03:55 +0200

    Updated readme.

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   version: '0.0.1',
   summary: 'Build Pipeline',
   git: 'https://github.com/MeteorPipeline/core',
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
